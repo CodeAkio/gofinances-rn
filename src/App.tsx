d9fcf49b6e05c883e2eb5e161f8d7561209fc7a9
CodeAkio <siqueira_2004@hotmail.com>
2022-01-23 15:36:15 -0300

    Config fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 import { ThemeProvider } from 'styled-components';
 
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+
 import theme from './global/styles/theme';
 import { Dashboard } from './pages/Dashboard';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
